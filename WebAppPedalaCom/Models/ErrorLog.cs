a017ba46bdda5da4273373b9156c6943e90d7e1b
Betacom <Betacom@BETACOM-PC329D>
2023-11-17 17:23:59 +0100

    initialize pedalacom api

---
+++ WebAppPedalaCom/Models/ErrorLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppPedalaCom.Models;
+
+public partial class ErrorLog
+{
+    public int ErrorLogId { get; set; }
+
+    public DateTime ErrorTime { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public int ErrorNumber { get; set; }
+
+    public string? ErrorProcedure { get; set; }
+
+    public string ErrorMessage { get; set; } = null!;
+}
