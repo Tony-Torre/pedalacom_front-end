e93cb8d85f626cbd2eb79566983d21e16dcfdda2
Betacom <Betacom@BETACOM-PC329D>
2023-11-22 10:18:58 +0100

    Added Products controller and API settings in program

--- WebAppPedalaCom/Program.cs
+++ WebAppPedalaCom/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebAppPedalaCom.Models;
+
 namespace WebAppPedalaCom
 {
     public class Program
@@ -8,13 +12,26 @@ namespace WebAppPedalaCom
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Add services to the container.
+            builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection2019")));
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddCors(opt => // to add for CORS Policy access!
+            {
+                opt.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader());
+            });
+
             var app = builder.Build();
+            app.UseCors("CorsPolicy");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
