2ff994cb23870defdacf03114cba7a07523b9bbb
D3ENNY <denysraimondi06@gmail.com>
2023-11-23 00:16:10 +0100

    upgrade sales section

---
+++ pedalacomApp/src/app/model/SalesSection/SalesSection.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saleSection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './SalesSection.component.html',
+  styleUrls: ['./SalesSection.component.scss']
+})
+export class SalesSectionComponent {
+
+  
+}
+
