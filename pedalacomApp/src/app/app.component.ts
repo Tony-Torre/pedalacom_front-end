2302b8607c51954366fc154cdddbf13eb9cedb2a
D3ENNY <denysraimondi06@gmail.com>
2023-11-22 16:09:19 +0100

    add discount Section

--- pedalacomApp/src/app/app.component.ts
+++ pedalacomApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DiscountedSectionComponent } from './model/DiscountedSection/DiscountedSection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbModule],
+  imports: [CommonModule, RouterOutlet, NgbModule, DiscountedSectionComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
