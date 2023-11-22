d24301630a5d4d74ff2637945f1cf6c9013b7929
CRTaipe <christian.romero02.cr@gmail.com>
2023-11-22 16:43:49 +0100

    Add Corusel component

--- pedalacomApp/src/app/app.component.ts
+++ pedalacomApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './model/carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbModule],
+  imports: [CommonModule, RouterOutlet, NgbModule, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
