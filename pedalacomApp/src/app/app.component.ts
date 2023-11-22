d83130f6a5a10c506fff26cddf7e7843312f4233
Betacom <Betacom@BETACOM-PC329D>
2023-11-22 11:19:15 +0100

    card style implemented

--- pedalacomApp/src/app/app.component.ts
+++ pedalacomApp/src/app/app.component.ts
@@ -1,12 +1,17 @@
+// IMPORT NATIVE COMPONENTS
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; // To import if API call in Child Module
 import { RouterOutlet } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // for SPA RouteLinks
+// IMPORT  CUSTOM COMPONENTS
+import { CardComponent } from './model/bikecard/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, HttpClientModule, NgbModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
