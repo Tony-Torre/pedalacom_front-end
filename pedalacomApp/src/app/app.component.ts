53379bc1508e0e283b9bb66e45cfb5351e466048
MyNameIsAlpaca <saragosagabriele@gmail.com>
2023-11-22 12:57:31 +0100

    Feat: add font-awesome and init of search

--- pedalacomApp/src/app/app.component.ts
+++ pedalacomApp/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './core/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbModule],
+  imports: [CommonModule, RouterOutlet, NgbModule,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -14,3 +16,4 @@ export class AppComponent {
   title = 'pedalacomApp';
   constructor(private NgbModule: NgbModule) { }
 }
+
