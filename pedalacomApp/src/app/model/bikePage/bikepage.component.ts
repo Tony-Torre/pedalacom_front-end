d6da6a592aa3762b32484fcec3acc72bc972f2f9
Betacom <Betacom@BETACOM-PC329D>
2023-11-29 12:10:57 +0100

    OKOK

---
+++ pedalacomApp/src/app/model/bikePage/bikepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bikepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bikepage.component.html',
+  styleUrl: './bikepage.component.scss'
+})
+export class BikepageComponent {
+
+}
