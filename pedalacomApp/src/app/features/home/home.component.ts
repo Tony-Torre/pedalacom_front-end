06f9ba93e65f0b923c043c5c47c65acde11afefa
Betacom <Betacom@BETACOM-PC329D>
2023-11-29 11:08:45 +0100

    pages main structure done

--- pedalacomApp/src/app/features/home/home.component.ts
+++ pedalacomApp/src/app/features/home/home.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // IMPORT CUSTOM COMPONENTS
 import { CarouselComponent } from '../../model/carousel/carousel.component';
-import { CardComponent } from '../../model/BikeCard/card.component';
 import { SalesSectionComponent } from '../../model/SalesSection/SalesSection.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CarouselComponent, CardComponent, SalesSectionComponent],
+  imports: [CommonModule, CarouselComponent, SalesSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
