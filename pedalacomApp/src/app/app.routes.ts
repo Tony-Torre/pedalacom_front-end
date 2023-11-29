1840644867158fab66b120c13de7693b33f28faa
Betacom <Betacom@BETACOM-PC329D>
2023-11-29 11:05:12 +0100

    updated routes and components

--- pedalacomApp/src/app/app.routes.ts
+++ pedalacomApp/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ProductsComponent } from './features/products/products.component';
 import { CartComponent } from './features/cart/cart.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'products', component: ProductsComponent},
     {path: 'cart', component: CartComponent},
+    {path: 'contacts', component: ContactsComponent},
+
     //{path:'', redirectTo: 'home', pathMatch: 'full'} redirect path!
 ];
--- pedalacomApp/src/app/features/products/products.component.ts
+++ pedalacomApp/src/app/features/products/products.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// IMPORT CUSTOM COMPONENTS
+import { CardComponent } from '../../model/BikeCard/card.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
