d221c931f3aae5e2e9deb94f0c67a88e35b904d8
MyNameIsAlpaca <saragosagabriele@gmail.com>
2023-12-06 17:35:25 +0100

    feat: add collegamento registrazionelogin al back

--- pedalacomApp/src/app/features/login/login.component.ts
+++ pedalacomApp/src/app/features/login/login.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomerApiServiceService } from '../../shared/CRUD/customer-api-service.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [CustomerApiServiceService]
 })
 export class LoginComponent {
+
+  constructor (private login: CustomerApiServiceService) {}
+
+  runLogin(email: string, password: string)
+  {
+    this.login.loginCustomer(email, password).subscribe((resp)=>{
+      if(resp.status == 200 || resp.status == 201){
+        console.log("sei loggato")
+        /* this.login.setLoggedToken(email, resp.body.customerId) */
+        console.log(resp)
+        
+      }else{
+        console.log("non sei loggato")
+      }
+    });
+  }
 
   
 }
